7cb20505dc0138af314df8ab6111e15af6c6d3ae
Azamat <azamat-nazmutdinov@yandex.ru>
2023-06-17 16:54:30 +0500

    seminar 3 homework

---
+++ exeptions/seminar3/InputPersonFormatException.java
@@ -0,0 +1,7 @@
+package seminar3;
+
+public class InputPersonFormatException extends Exception {
+    public InputPersonFormatException(String message) {
+        super("Ошибка введенных данных: " + message);
+    }
+}
